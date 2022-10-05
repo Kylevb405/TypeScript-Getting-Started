d215532cb1fdca943851b4d35903ae7e8ac35058
Kyle Van Buren <kylevanburen405@gmail.com>
2022-10-05 00:30:23 -0700

    Adding Type Annotations and Default Parameter Values

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,43 @@
 function startGame() {
 
-    let playerName: string = 'Audrey';
+    let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
 
-    let messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    postScore(100, playerName);
+
+    // let messagesElement = document.getElementById('messages');
+    // messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
-function logPlayer(name) {
+function logPlayer(name: string = 'Mulitmath Player'): void {
     console.log(`New game starting for player: ${name}`);
 }
 
+function getInputValue(elementID: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if (inputElement.value === '') {
+        return undefined;
+    } else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'):void {
+    const scoreElement:HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`
+}
+
 document.getElementById('startgame')!.addEventListener('click', startGame);
+
+
+
+
+
+
+// function funFunc (score: number, secondNum: number): [number, number] {
+//     return [score, secondNum]
+   
+// }
+// const newMessage : [number, number] = funFunc(4, 89);  //  "Congradulations!. Score: 4"
+// "89. Score: 4"
